31d47caf6c0c164625ff7ecb1571dd696fca2962
Meri Mkhitaryan <meri.mkhitaryan03@gmail.com>
2024-11-29 23:55:49 +0400

    Longest Text

---
+++ 23-11-24/index3.js
@@ -0,0 +1,12 @@
+// Given an array of strings ["React", "JavaScript", "Redux", "PHP", "C#"]:
+// Write a program to find the longest word in the array.
+// Solution in JavaScript using only for loop and arrays:
+
+const arr=["React", "JavaScript", "Redux", "PHP", "C#"];
+let longItem=arr[0];
+for (let i=1;i<arr.length;i++){
+    if(arr[i].length>longItem.length){
+        longItem=arr[i];
+    }
+}
+console.log(longItem)
