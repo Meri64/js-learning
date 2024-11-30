4728f5713b07e5a192cab37ee3bb64187c4e2a4a
Meri Mkhitaryan <meri.mkhitaryan03@gmail.com>
2024-11-30 10:59:53 +0400

    Calculator

---
+++ 16-11-24/index2.js
@@ -0,0 +1,20 @@
+let num1 = 5;
+let num2 = 3;
+let  operation = '*';
+switch(operation){
+    case '+':
+        console.log (num1+num2);
+        break;
+    case '-':
+        console.log(num1-num2);
+        break;
+    case '*':
+        console.log (num1*num2);
+        break;
+    case '/':
+        console.log(num1/num2); 
+        break;
+    default:
+        console.log('invalid operator')
+
+}
