798f37a33b8c36643f41ad7f068262d38b86e177
Meri Mkhitaryan <meri.mkhitaryan03@gmail.com>
2024-11-30 10:56:24 +0400

    even or odd number decision

---
+++ 16-11-24/index1.js
@@ -0,0 +1,13 @@
+// Write a program that takes a number as input and checks if it is even or odd:
+// If the number is even, print “The number is even.”
+// If the number is odd, print “The number is odd.”
+// Use and learn modulo operator %
+
+let  userNumber=48;
+userNumber=Number(userNumber);
+if (!isNaN(userNumber) && userNumber % 2===0){
+
+console.log('the number is even');
+} else {
+ console.log('this is odd number')
+    }
