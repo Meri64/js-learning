57bd24cd92fb07c81feae23e9455e99c4fa4b3ad
Meri Mkhitaryan <meri.mkhitaryan03@gmail.com>
2024-11-30 11:09:01 +0400

    Grades check

---
+++ 16-11-24/index4.js
@@ -0,0 +1,15 @@
+let  userScore='cdvrev';
+if (userScore>=90 && userScore<=100) {
+console.log('A');}
+else if (userScore>=80 && userScore<=89) {
+console.log('B');}
+else if (userScore>=70 && userScore<=79) {
+console.log('C');}
+    else if (userScore>=60 && userScore<=69) {
+console.log ('D');}
+else if (userScore>=0 && userScore<=59) {
+console.log ('F');}
+else {
+console.log('invalid score');
+
+}
